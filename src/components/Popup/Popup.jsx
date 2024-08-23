682a9064b2f201651b36c5c3dc611af3f2b2c67a
Irina Shorokhova <irinaais90@gmail.com>
2024-08-23 20:30:53 +0300

    fix: удалила лишнюю кнопку

--- src/components/Popup/Popup.jsx
+++ src/components/Popup/Popup.jsx
@@ -35,15 +35,12 @@ export default function Popup({ isOpen, onClose, content }) {
   }
 
   function handleOverlayClick(evt) {
-    if (evt.target === evt.currentTarget) {
-      onClose();
-    }
+    if (evt.target === evt.currentTarget) onClose();
   }
 
   return (
     <div className={`popup ${isOpen && 'popup_opened'}`} onClick={handleOverlayClick}>
       <div className="popup__view-card">
-        <button className="popup__close-button" onClick={onClose}>Назад</button>
         <p>{content}</p>
       </div>
     </div>
