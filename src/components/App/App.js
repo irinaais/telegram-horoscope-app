87647f64643a4d11baa6c4a32e2f2824215d809f
Irina Shorokhova <irinaais90@gmail.com>
2024-08-23 18:03:18 +0300

    feat: добавила кнопку переключения языка

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,31 @@
 import './App.css';
 import {zodiacSignsEN, zodiacSignsRU} from '../../utils/constants';
 import Sign from '../Sign/Sign';
+import { useState } from 'react';
 
 function App() {
-  const language= 'RU';
+  const [language, setLanguage] = useState('RU');
   const listOfHoroscope= language === 'RU' ? zodiacSignsRU : zodiacSignsEN;
   const userLanguage = language === 'RU' ? 'original' : 'translated';
 
+  const toggleLanguage = () => {
+    setLanguage(prevLanguage => (prevLanguage === 'RU' ? 'EN' : 'RU'));
+  };
+
   return (
     <main className='main'>
-      {listOfHoroscope.map((sign, index) => {
-        return <Sign
-          key={index}
-          sign={sign}
-          language={userLanguage}
-        />
-      })}
+      <button className='language-toggle' onClick={toggleLanguage}>
+        {language === 'RU' ? 'Switch to English' : 'Переключиться на русский'}
+      </button>
+      <div className='signs__container'>
+        {listOfHoroscope.map((sign, index) => {
+          return <Sign
+            key={index}
+            sign={sign}
+            language={userLanguage}
+          />
+        })}
+      </div>
     </main>
   );
 }
